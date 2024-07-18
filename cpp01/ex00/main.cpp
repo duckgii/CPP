8690bc79cf008327c21e3cc519ac2b8a6e950719
Yeonggi Shin <yeoshin@c2r3s6.42seoul.kr>
2024-07-18 20:51:10 +0900

    Feat : 00 finish

---
+++ cpp01/ex00/main.cpp
@@ -0,0 +1,27 @@
+#include "Zombie.hpp"
+#include <sstream>
+
+Zombie* newZombie(std::string name);
+void	randomChump(std::string name);
+
+int main()
+{
+	Zombie	*crr;
+	std::stringstream num;
+
+	for (int i = 0; i < 10; i++)
+	{
+		num << i;
+		crr = newZombie("Sungyoon" + num.str());
+		crr->announce();
+		delete(crr);
+		num.str("");
+	}
+	for (int i = 0; i < 10; i++)
+	{
+		num << i;
+		randomChump("Sungyoon_ver" + num.str());
+		num.str("");
+	}
+	return (0);
+}
