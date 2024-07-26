5a7b88d4c71baf1f706abfc0b3369422c5281488
Yeonggi Shin <yeoshin@c2r3s5.42seoul.kr>
2024-07-26 15:53:06 +0900

    Feat : ex02 is implemented

--- cpp02/ex02/Fixed.cpp
+++ cpp02/ex02/Fixed.cpp
@@ -2,29 +2,29 @@
 
 Fixed::Fixed() : number(0)
 {
-	std::cout<<"Default constructor called"<<std::endl;
+	//std::cout<<"Default constructor called"<<std::endl;
 }
 
 Fixed::Fixed(const int input)
 {
-	std::cout<<"Int constructor called"<<std::endl;
+	//std::cout<<"Int constructor called"<<std::endl;
 	number = input << myraw;
 }
 
 Fixed::Fixed(const float input)
 {
-	std::cout<<"Float constructor called"<<std::endl;
+	//std::cout<<"Float constructor called"<<std::endl;
 	number = roundf(input * 256);
 }
 
 Fixed::~Fixed()
 {
-	std::cout<<"Destructor called"<<std::endl;
+	//std::cout<<"Destructor called"<<std::endl;
 }
 
 Fixed::Fixed(const Fixed &copy)
 {
-	std::cout<<"Copy constructor called"<<std::endl;
+	//std::cout<<"Copy constructor called"<<std::endl;
 	*this = copy;
 }
 
@@ -33,10 +33,10 @@ Fixed&	Fixed::operator=(const Fixed &in)
 
 	if (this == &in)
 	{
-		std::cout<<"Invalid input!"<<std::endl;
+		//std::cout<<"Invalid input!"<<std::endl;
 		return (*this);
 	}
-	std::cout<<"Copy assignment operator called"<<std::endl;
+	//std::cout<<"Copy assignment operator called"<<std::endl;
 	number = in.getRawBits();
 	return (*this);
 }
@@ -47,9 +47,88 @@ std::ostream&	operator<<(std::ostream &out, const Fixed &in)
 	return (out);
 }
 
+Fixed&	Fixed::operator++()
+{
+	number += 1;
+	return (*this);
+}
+
+const Fixed	Fixed::operator++(int)
+{
+	const Fixed	retobj(this->toFloat());
+	number += 1;
+	return (retobj);
+}
+
+Fixed&	Fixed::operator--()
+{
+	number -= 1;
+	return (*this);
+}
+
+const Fixed	Fixed::operator--(int)
+{
+	Fixed	retobj(this->toFloat());
+	number -= 1;
+	return (retobj);
+}
+
+Fixed	Fixed::operator+(const Fixed &in) const
+{
+	Fixed	ret(this->toFloat() + in.toFloat());
+	return (ret);
+}
+
+Fixed	Fixed::operator-(const Fixed &in) const
+{
+	Fixed	ret(this->toFloat() - in.toFloat());
+	return (ret);
+}
+
+Fixed	Fixed::operator*(const Fixed &in) const
+{
+	Fixed	ret(this->toFloat() * in.toFloat());
+	return (ret);
+}
+
+Fixed	Fixed::operator/(const Fixed &in) const
+{
+	Fixed	ret(this->toFloat() / in.toFloat());
+	return (ret);
+}
+
+int	Fixed::operator>(const Fixed &in) const
+{
+	return(this->number > in.number);
+}
+
+int	Fixed::operator<(const Fixed &in) const
+{
+	return(this->number < in.number);
+}
+
+int	Fixed::operator<=(const Fixed &in) const
+{
+	return(this->number <= in.number);
+}
+
+int	Fixed::operator>=(const Fixed &in) const
+{
+	return(this->number >= in.number);
+}
+
+int	Fixed::operator==(const Fixed &in) const
+{
+	return(this->number == in.number);
+}
+
+int	Fixed::operator!=(const Fixed &in) const
+{
+	return(this->number != in.number);
+}
+
 int	Fixed::getRawBits(void) const
 {
-	//std::cout<<"getRawBits member function called"<<std::endl;
 	return (number);
 }
 
@@ -66,4 +145,33 @@ int	Fixed::toInt(void) const
 float	Fixed::toFloat(void) const
 {
 	return (getRawBits() / (float)256);
+}
+
+Fixed&	Fixed::min(Fixed& a, Fixed& b)
+{
+	if (a.number >= b.number)
+		return (b);
+	return (a);
+}
+
+const Fixed&	Fixed::min(const Fixed& a, const Fixed& b)
+{
+	Fixed	ret(0);
+	if (a.number >= b.number)
+		return (b);
+	return (a);
+}
+
+Fixed&	Fixed::max(Fixed& a, Fixed& b)
+{
+	if (a.number >= b.number)
+		return (a);
+	return (b);
+}
+
+const Fixed&	Fixed::max(const Fixed& a, const Fixed& b)
+{
+	if (a.number >= b.number)
+		return (a);
+	return (b);
 }
--- cpp02/ex02/Fixed.hpp
+++ cpp02/ex02/Fixed.hpp
@@ -3,6 +3,7 @@
 
 #include <iostream>
 #include <cmath>
+#include <algorithm>
 
 class Fixed
 {
@@ -16,13 +17,31 @@ class Fixed
 		Fixed(const Fixed &copy);
 		~Fixed();
 
-		Fixed&		operator=(const Fixed &in);
+		Fixed&	operator=(const Fixed &in);
+		Fixed&	operator++();
+		Fixed&	operator--();
+		const Fixed	operator++(int);
+		const Fixed	operator--(int);
+		Fixed	operator*(const Fixed &in) const;
+		Fixed	operator/(const Fixed &in) const;
+		Fixed	operator+(const Fixed &in) const;
+		Fixed	operator-(const Fixed &in) const;
+		int	operator<(const Fixed &in) const;
+		int	operator>(const Fixed &in) const;
+		int	operator<=(const Fixed &in) const;
+		int	operator>=(const Fixed &in) const;
+		int	operator==(const Fixed &in) const;
+		int	operator!=(const Fixed &in) const;
+
 		int		getRawBits(void) const;
 		void	setRawBits(int const raw);
 		float	toFloat(void) const;
 		int		toInt(void) const;
+		static Fixed&	min(Fixed& a, Fixed& b);
+		static const Fixed&	min(const Fixed& a, const Fixed& b);
+		static Fixed&	max(Fixed& a, Fixed& b);
+		static const Fixed&	max(const Fixed& a, const Fixed& b);
 };
 
 std::ostream&	operator<<(std::ostream &out, const Fixed &in);
-
 #endif
