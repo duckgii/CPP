b456da3f43d2bf19a5498e285859c4dd0c902364
Yeonggi Shin <yeoshin@c2r3s5.42seoul.kr>
2024-07-26 19:11:17 +0900

    Feat : add example

--- cpp02/ex03/main.cpp
+++ cpp02/ex03/main.cpp
@@ -2,7 +2,11 @@
 
 int main( void )
 {
-	std::cout<<bsp(Point(0,0), Point(0,5), Point(5,0), Point(1,1))<<std::endl;
-	std::cout<<bsp(Point(0,0), Point(0,5), Point(5,0), Point(2,0))<<std::endl;
+	std::cout<<"in : "<<bsp(Point(0,0), Point(0,5), Point(5,0), Point(1,1))<<std::endl;
+	std::cout<<"out : "<<bsp(Point(0,0), Point(0,5), Point(5,0), Point(2,0))<<std::endl;
+	std::cout<<"out : "<<bsp(Point(0,0), Point(0,5), Point(5,0), Point(-1,3))<<std::endl;
+	std::cout<<"in : "<<bsp(Point(0,0), Point(0,10), Point(5,5), Point(3,5))<<std::endl;
+	std::cout<<"out : "<<bsp(Point(0,0), Point(0,10), Point(5,5), Point(0,10))<<std::endl;
+	std::cout<<"out : "<<bsp(Point(0,0), Point(0,10), Point(5,5), Point(10,1))<<std::endl;
 	return (0);
 }
