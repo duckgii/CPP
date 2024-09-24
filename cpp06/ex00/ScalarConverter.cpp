1f2f5a26f1589c8d9ca765c2fb8acb11e7243ed3
duckgii <shinyonggi@naver.com>
2024-09-24 21:31:47 +0900

    fix: error

--- cpp06/ex00/ScalarConverter.cpp
+++ cpp06/ex00/ScalarConverter.cpp
@@ -61,37 +61,49 @@ void	ScalarConverter::convert(const char *input)
 	double num = std::strtod(input, 0);
 	std::string	in = input;
 
-	if (num == 0.0 || checkValidNumber(input) == false)
+	if (in != "nan" && ((in[0] != '0' && num == 0.0) || checkValidNumber(input) == false))
 	{
 		std::cout<<"Input value is invalid!!!!!!"<<std::endl;
 		return;
 	}
-	std::cout.precision(10);	
+	std::cout.precision(10);
+
 	std::cout<<"char : ";
-	if (std::isprint(static_cast<int>(num)))
-		std::cout<<static_cast<char>(static_cast<int>(num))<<std::endl;
+	if (in == "nan")
+		std::cout<<"impossible"<<std::endl;
+	else if (std::isprint(static_cast<int>(num)))
+		std::cout<<"'"<<static_cast<char>(num)<<"'"<<std::endl;
+	else if  (num > CHAR_MAX)
+		std::cout<<"impossible"<<std::endl;
 	else
 		std::cout<<"Non displayable"<<std::endl;
+		
 	
 	std::cout<<"int : ";
-	if (num > INT_MAX || num < INT_MIN)
+	if (in == "nan")
+		std::cout<<"impossible"<<std::endl;
+	else if (num > INT_MAX || num < INT_MIN)
 		std::cout<<"impossible"<<std::endl;
 	else
 		std::cout<<static_cast<int>(num)<<std::endl;
 
 	std::cout<<"float : ";
-	if (num > FLT_MAX)
+	if (in == "nan")
+		std::cout<<"nanf"<<std::endl;
+	else if (num > FLT_MAX)
 		std::cout<<"+inff"<<std::endl;
 	else if (num < -1 * FLT_MAX)
 		std::cout<<"-inff"<<std::endl;
 	else
-		std::cout<<num<<((in.find(".") ==  std::string::npos) ? ".0f" : "f")<<std::endl;
+		std::cout<<num<<((static_cast<int>(num) ==  num) ? ".0f" : "f")<<std::endl;
 
 	std::cout<<"double : ";
-	if (num > FLT_MAX && num > 0)
+	if (in == "nan")
+		std::cout<<"nan"<<std::endl;
+	else if (num > FLT_MAX && num > 0)
 		std::cout<<"inf"<<std::endl;
-	if (num < FLT_MIN)
+	else if (num < -1 * FLT_MIN)
 		std::cout<<"-inf"<<std::endl;
 	else
-		std::cout<<static_cast<double>(num)<<((in.find(".") ==  std::string::npos) ? ".0" : "")<<std::endl;
+		std::cout<<static_cast<double>(num)<<((static_cast<int>(num) ==  num) ? ".0" : "")<<std::endl;
 }
