abc90927a2480ec93287a973aa62a530d91b799e
duckgii <shinyonggi@naver.com>
2024-09-27 12:36:05 +0900

    Fix : minus error

--- cpp06/ex00/ScalarConverter.cpp
+++ cpp06/ex00/ScalarConverter.cpp
@@ -66,12 +66,12 @@ void	ScalarConverter::convert(const char *input)
 		std::cout<<"Input value is invalid!!!!!!"<<std::endl;
 		return;
 	}
-	std::cout.precision(5);
+	std::cout.precision(10);
 
 	std::cout<<"char : ";
 	if (in == "nan")
 		std::cout<<"impossible"<<std::endl;
-	else if  (num > CHAR_MAX)
+	else if  (num > CHAR_MAX || num < CHAR_MIN)
 		std::cout<<"impossible"<<std::endl;
 	else if (std::isprint(static_cast<char>(static_cast<int>(num)))) // double을 바로 char로 바꾸려고 하면 에러가 발생함 ->큰 값인 부동소수점 -> 작은 값인 고정소수점이라서. int형으로 중간에 한번 바꿔주는게 안정적임.
 		std::cout<<"'"<<static_cast<char>(static_cast<int>(num))<<"'"<<std::endl;
@@ -100,9 +100,9 @@ void	ScalarConverter::convert(const char *input)
 	std::cout<<"double : ";
 	if (in == "nan")
 		std::cout<<"nan"<<std::endl;
-	else if (num > DBL_MAX && num > 0)
+	else if (num > DBL_MAX)
 		std::cout<<"inf"<<std::endl;
-	else if (num < -1 * DBL_MIN)
+	else if (num < -1 * DBL_MAX)
 		std::cout<<"-inf"<<std::endl;
 	else
 		std::cout<<static_cast<double>(num)<<((static_cast<int>(num) ==  num) ? ".0" : "")<<std::endl;
